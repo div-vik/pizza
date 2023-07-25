f89ebc2395a0a4179addd65ce0cd6215e6731598
SidZhoop <siddharth@zhoop.in>
2023-07-25 19:39:28 +0530

    notification completed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Notification = () => {
   return (
-    <div className="bg-red-500 px-4 flex items-center justify-center text-center text-sm text-white h-12 md:text-base cursor-pointer">
+    <div className="px-4 bg-red-500 flex items-center justify-center text-center text-sm text-white h-12 md:text-base cursor-pointer">
       Free delivery for all orders over Rs.400. Order your food now!
     </div>
   );
